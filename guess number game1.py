b2c3a2abb8ce3e09725e7d094dcbe7e56713eb40
11lcb <13525090273@163.com>
2025-09-10 16:07:31 +0300

    guess number game2

--- guess number game1.py
+++ guess number game1.py
@@ -1,12 +1,28 @@
 import random
 a=random.randint(1,100)
-counts=10
+counts=8
 
 while counts>0:
     A=input("give me a number between 1 to 100 \n \n")
     B=int(A)
     if B==a :
-        print("right \n"*3)
+        print(" right \n ur such a genius! \n lets try a harder one")
+        c=random.randint(1,2000)
+        times=20
+        while times>0:
+            C=input("\nthe same rules,this time between 1 to 2000\n")
+            D=int(C)
+            if D==c:
+                print("\nwhat the hell, bro!!\n goodjob! \nareu kidding me?\n")
+                break
+            else:
+                if D<c:
+                    print("\n give me a bigger one")
+                if D>c:
+                    print("\nok,too big\n")
+                if D>2000:
+                    print("\n come on bro,under 2000!:(\n")
+            times=times-1                
         break
     else:
         if B<a :
@@ -17,5 +33,8 @@ while counts>0:
             print("\n between 1 to 100, remember?\n")    
         print("\n try it again!\n")    
     counts=counts-1
+while counts<=0:
+    print("\ngame over:(\n seems like u should exercise more")   
+    break 
 print(":)")                
 
